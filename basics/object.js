3375c2957d87e6e58079b289704288a1abc9a00a
natsumadan <madan.mohit209@gmail.com>
2021-03-16 05:14:55 +1000

    update

---
+++ basics/object.js
@@ -0,0 +1,20 @@
+const backpack={
+    name:"Every Backpack",
+    volume:30,
+    pocketNum: 15,
+    color: "grey",
+    strapLength:{
+        left:26,
+        right:26,
+    },
+    lidOpen: false,
+    toggleLid: function(lidStatus){
+        this.lidOpen=lidStatus;
+    },
+    newStrapLength: function(lengthLeft, lengthRight){
+        this.strapLength.left=lengthLeft;
+        this.strapLength.right=lengthRight;
+    }
+
+
+};
