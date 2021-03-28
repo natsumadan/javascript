73ff5864acc4dbaf4c8e751800e57df7f019a534
natsumadan <madan.mohit209@gmail.com>
2021-03-28 23:29:36 +1000

    update

---
+++ globalObject/script.js
@@ -0,0 +1,18 @@
+import Backpack from "./backpack.js"
+
+
+const EverydayBackpack = new Backpack (
+    "mohitBag",
+    20, 
+    "blue",
+    15,
+    15,
+    25,
+    "false",
+    "11 November 2014",
+
+)
+
+
+console.log("The is the poclet numebr", EverydayBackpack.pocketNum);
+console.log("this is the date", EverydayBackpack.dateRequired); 
